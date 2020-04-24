4bb091f59f987d7ab26e0f9f319a453d91fb69b0
Dallore <misha_teddy@rambler.ru>
2020-04-24 10:34:38 +0300

    complete more correctly

--- src/homework/Task2.java
+++ src/homework/Task2.java
@@ -1,23 +1,36 @@
 package homework;
 
-import java.util.Scanner;
 
 public class Task2 {
 
-    public void main ( String[] args ) {
+    public static void main ( String[] args ) {
 
         System.out.println ( Calculation ( 10 , '+' , 10 ) );
+        System.out.println ( Calculation ( 10 , '-' , 10 ) );
+        System.out.println ( Calculation ( 10 , '*' , 10 ) );
+        System.out.println ( Calculation ( 10 , '/' , 10 ) );
 
     }
 
-
-    public static double Calculation ( int n1 , char sign , int n2 ) {
+    public static int Calculation ( int n1 , char sign , int n2 ) {
         int a = 0;
 
         if (sign == '+') {
             a = n1 + n2;
-            System.out.println (a);
         }
+
+        if (sign == '-') {
+            a = n1 - n2;
+        }
+
+        if (sign == '*') {
+            a = n1 * n2;
+        }
+
+        if (sign == '/') {
+            a = n1 / n2;
+        }
+
         return a;
     }
 }
---
+++ src/homework9/Task1.java
@@ -0,0 +1,15 @@
+package homework9;
+
+public class Task1 {
+
+    static int counter;
+
+    public static void main ( String[] args ) {
+
+    }
+
+    public static class NextCount{
+
+    }
+
+}
